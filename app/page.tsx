505e758bec894f6530e04f14bc8d795dd6bf19af
aminulds <aminulds61@gmail.com>
2024-01-30 14:28:45 +0600

    login button conditional render

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
 import Image from 'next/image';
 
 export default function Page() {
+  const isSignIn = false;
+
   return (
     <main className="flex min-h-screen flex-col p-4">
       <div className="flex h-20 shrink-0 items-center gap-8 rounded-lg bg-indigo-500 p-4 md:h-52">
-        <AcmeLogo />
-        <Link href='/dashboard' className='text-2xl text-white font-medium'>Dashboard</Link>
+        <Link href='/'><AcmeLogo /></Link>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -20,12 +20,23 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-indigo-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-indigo-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          
+          {isSignIn ? (
+            <Link
+              href="/dashboard"
+              className="flex items-center gap-5 self-start rounded-lg bg-indigo-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-indigo-400 md:text-base"
+            >
+              <span>Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+          ): (
+            <Link
+              href="/login"
+              className="flex items-center gap-5 self-start rounded-lg bg-indigo-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-indigo-400 md:text-base"
+            >
+              <span>Log In</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+          )}
+
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
