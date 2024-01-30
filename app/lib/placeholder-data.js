7fe8e2efb0a6156798639cd1e8f07c1c0ac8a768
aminulds <aminulds61@gmail.com>
2024-01-30 13:25:47 +0600

    more data added

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -70,6 +70,306 @@ const customers = [
     email: 'balazs@orban.com',
     image_url: '/customers/balazs-orban.png',
   },
+  {
+    id: 'a5bc044c-792e-4295-b77c-63647e612700',
+    name: 'Random User 1',
+    email: 'random.user.1@gmail.com',
+    image_url: '/customers/delba-de-oliveira.png'
+  },
+  {
+    id: '688cfa20-fc21-4d42-829a-2746c37bf771',
+    name: 'Random User 2',
+    email: 'random.user.2@yahoo.com',
+    image_url: '/customers/lee-robinson.png'
+  },
+  {
+    id: '0bc9ee5d-462e-4174-95c3-c69b1c18b13e',
+    name: 'Random User 3',
+    email: 'random.user.3@yahoo.com',
+    image_url: '/customers/hector-simpson.png'
+  },
+  {
+    id: '528412dd-be78-4763-8d8d-a2078c9002ca',
+    name: 'Random User 4',
+    email: 'random.user.4@yahoo.com',
+    image_url: '/customers/steven-tey.png'
+  },
+  {
+    id: 'd8bd7a07-94bc-4c9c-8498-525e6d72bbdf',
+    name: 'Random User 5',
+    email: 'random.user.5@yahoo.com',
+    image_url: '/customers/steph-dietz.png'
+  },
+  {
+    id: 'c2fc2975-920e-4da3-bea4-27e6d56ab726',
+    name: 'Random User 6',
+    email: 'random.user.6@yahoo.com',
+    image_url: '/customers/michael-novotny.png'
+  },
+  {
+    id: '8f880a16-0349-47ae-af68-d8d3937bf6fa',
+    name: 'Random User 7',
+    email: 'random.user.7@outlook.com',
+    image_url: '/customers/evil-rabbit.png'
+  },
+  {
+    id: '84aef5c6-e868-4830-a750-b1dc74619f20',
+    name: 'Random User 8',
+    email: 'random.user.8@yahoo.com',
+    image_url: '/customers/emil-kowalski.png'
+  },
+  {
+    id: '5795443a-a47e-407e-bc1a-7b7aedc73733',
+    name: 'Random User 9',
+    email: 'random.user.9@yahoo.com',
+    image_url: '/customers/amy-burns.png'
+  },
+  {
+    id: 'cdb804ef-f6e5-4649-8200-d5004c70f154',
+    name: 'Random User 10',
+    email: 'random.user.10@outlook.com',
+    image_url: '/customers/balazs-orban.png'
+  },
+  {
+    id: 'ecb9a2ec-4279-4f7e-96f6-899a3c3a51f3',
+    name: 'Random User 11',
+    email: 'random.user.11@gmail.com',
+    image_url: '/customers/delba-de-oliveira.png'
+  },
+  {
+    id: '988eada5-8aea-40e5-a363-486388206fb4',
+    name: 'Random User 12',
+    email: 'random.user.12@hotmail.com',
+    image_url: '/customers/lee-robinson.png'
+  },
+  {
+    id: '140267e1-6149-43d0-9289-7e9023bf912e',
+    name: 'Random User 13',
+    email: 'random.user.13@hotmail.com',
+    image_url: '/customers/hector-simpson.png'
+  },
+  {
+    id: 'bc728350-5d6b-435d-bd0c-584e65354714',
+    name: 'Random User 14',
+    email: 'random.user.14@yahoo.com',
+    image_url: '/customers/steven-tey.png'
+  },
+  {
+    id: '7e075a35-5126-4015-bc92-84dc2080ba3a',
+    name: 'Random User 15',
+    email: 'random.user.15@yahoo.com',
+    image_url: '/customers/steph-dietz.png'
+  },
+  {
+    id: '8f887845-0c79-46f0-9268-af2c1a4b53fd',
+    name: 'Random User 16',
+    email: 'random.user.16@hotmail.com',
+    image_url: '/customers/michael-novotny.png'
+  },
+  {
+    id: 'f4db000c-ee7d-4dac-b1b4-ce383e0af272',
+    name: 'Random User 17',
+    email: 'random.user.17@hotmail.com',
+    image_url: '/customers/evil-rabbit.png'
+  },
+  {
+    id: '32cf82a3-30c5-45c3-ac9c-76a81b491099',
+    name: 'Random User 18',
+    email: 'random.user.18@yahoo.com',
+    image_url: '/customers/emil-kowalski.png'
+  },
+  {
+    id: 'ecea1e04-eeeb-4c09-9102-11021c30caf3',
+    name: 'Random User 19',
+    email: 'random.user.19@yahoo.com',
+    image_url: '/customers/amy-burns.png'
+  },
+  {
+    id: '8ccd7ee8-98e7-46e9-9e21-a4006b33aebe',
+    name: 'Random User 20',
+    email: 'random.user.20@yahoo.com',
+    image_url: '/customers/balazs-orban.png'
+  },
+  {
+    id: '353ffc40-3820-4307-bf8b-ee9799fdfd4b',
+    name: 'Random User 21',
+    email: 'random.user.21@outlook.com',
+    image_url: '/customers/delba-de-oliveira.png'
+  },
+  {
+    id: 'b6f63fe9-8664-4102-865e-0427c4b8cb6b',
+    name: 'Random User 22',
+    email: 'random.user.22@outlook.com',
+    image_url: '/customers/lee-robinson.png'
+  },
+  {
+    id: 'c59da98d-7bc6-4fc0-bc15-f1a14616ffbd',
+    name: 'Random User 23',
+    email: 'random.user.23@yahoo.com',
+    image_url: '/customers/hector-simpson.png'
+  },
+  {
+    id: '90eeaa92-74ad-4474-bf0e-e9f81252b32c',
+    name: 'Random User 24',
+    email: 'random.user.24@outlook.com',
+    image_url: '/customers/steven-tey.png'
+  },
+  {
+    id: 'a5e3985b-cfe0-4b83-b4a6-b7c5b40e5577',
+    name: 'Random User 25',
+    email: 'random.user.25@hotmail.com',
+    image_url: '/customers/steph-dietz.png'
+  },
+  {
+    id: 'a22228a9-b1b6-4a7e-b317-eb24ab110371',
+    name: 'Random User 26',
+    email: 'random.user.26@outlook.com',
+    image_url: '/customers/michael-novotny.png'
+  },
+  {
+    id: '43dba3c3-3917-4945-a59e-8e15f933f72a',
+    name: 'Random User 27',
+    email: 'random.user.27@gmail.com',
+    image_url: '/customers/evil-rabbit.png'
+  },
+  {
+    id: '613e5bb1-f1cb-489c-96d1-291952d51165',
+    name: 'Random User 28',
+    email: 'random.user.28@yahoo.com',
+    image_url: '/customers/emil-kowalski.png'
+  },
+  {
+    id: '98f0bd93-73ca-46ec-b187-4071760fd186',
+    name: 'Random User 29',
+    email: 'random.user.29@hotmail.com',
+    image_url: '/customers/amy-burns.png'
+  },
+  {
+    id: '18cc013d-75dd-4d17-ae61-09ad2e4446d4',
+    name: 'Random User 30',
+    email: 'random.user.30@yahoo.com',
+    image_url: '/customers/balazs-orban.png'
+  },
+  {
+    id: '74886bde-5c9a-456c-b3bb-c425f0af70f7',
+    name: 'Random User 31',
+    email: 'random.user.31@yahoo.com',
+    image_url: '/customers/delba-de-oliveira.png'
+  },
+  {
+    id: '341c7b04-16b6-4fb7-9dd8-60497df26114',
+    name: 'Random User 32',
+    email: 'random.user.32@hotmail.com',
+    image_url: '/customers/lee-robinson.png'
+  },
+  {
+    id: '5831d16a-d9f5-44a7-a0a0-f2e3ee013c1f',
+    name: 'Random User 33',
+    email: 'random.user.33@hotmail.com',
+    image_url: '/customers/hector-simpson.png'
+  },
+  {
+    id: 'f0fbeaa7-ab90-439c-a3fd-c17b51b2f216',
+    name: 'Random User 34',
+    email: 'random.user.34@outlook.com',
+    image_url: '/customers/steven-tey.png'
+  },
+  {
+    id: '6538b37c-1472-44e1-8138-093313829bd7',
+    name: 'Random User 35',
+    email: 'random.user.35@hotmail.com',
+    image_url: '/customers/steph-dietz.png'
+  },
+  {
+    id: '7dc17d44-4877-4c41-a32b-3572cafc3a27',
+    name: 'Random User 36',
+    email: 'random.user.36@yahoo.com',
+    image_url: '/customers/michael-novotny.png'
+  },
+  {
+    id: 'a6838a6d-2c16-4ebd-9efa-310a911197fc',
+    name: 'Random User 37',
+    email: 'random.user.37@hotmail.com',
+    image_url: '/customers/evil-rabbit.png'
+  },
+  {
+    id: 'bf67b8df-5f0a-4387-836e-f5eb29221ffa',
+    name: 'Random User 38',
+    email: 'random.user.38@yahoo.com',
+    image_url: '/customers/emil-kowalski.png'
+  },
+  {
+    id: 'b2e2b6ae-6d80-4e86-93e2-f53b1df3d185',
+    name: 'Random User 39',
+    email: 'random.user.39@hotmail.com',
+    image_url: '/customers/amy-burns.png'
+  },
+  {
+    id: '18dd910d-ca1c-4e2f-a70a-619167461238',
+    name: 'Random User 40',
+    email: 'random.user.40@yahoo.com',
+    image_url: '/customers/balazs-orban.png'
+  },
+  {
+    id: '844e344e-515a-45b9-a3f2-f8253449458d',
+    name: 'Random User 41',
+    email: 'random.user.41@gmail.com',
+    image_url: '/customers/delba-de-oliveira.png'
+  },
+  {
+    id: 'ae57904e-08b0-4ac8-a447-feb43b9ba41a',
+    name: 'Random User 42',
+    email: 'random.user.42@hotmail.com',
+    image_url: '/customers/lee-robinson.png'
+  },
+  {
+    id: '19fc05e5-b11a-45f6-b317-3c3b5f31c7a8',
+    name: 'Random User 43',
+    email: 'random.user.43@gmail.com',
+    image_url: '/customers/hector-simpson.png'
+  },
+  {
+    id: 'cd461345-fb58-4dd0-b1ef-96e396e3b105',
+    name: 'Random User 44',
+    email: 'random.user.44@outlook.com',
+    image_url: '/customers/steven-tey.png'
+  },
+  {
+    id: 'aa8ad85d-2a22-4936-bb59-8cbb134a1e7d',
+    name: 'Random User 45',
+    email: 'random.user.45@hotmail.com',
+    image_url: '/customers/steph-dietz.png'
+  },
+  {
+    id: '1a66d058-957b-4a7e-a2ca-5150938a5838',
+    name: 'Random User 46',
+    email: 'random.user.46@yahoo.com',
+    image_url: '/customers/michael-novotny.png'
+  },
+  {
+    id: 'ddbdb39c-c30a-484a-ba7f-3520a52f2024',
+    name: 'Random User 47',
+    email: 'random.user.47@yahoo.com',
+    image_url: '/customers/evil-rabbit.png'
+  },
+  {
+    id: '37b4d0d2-b5c6-4070-aef8-ae7d22d6bc19',
+    name: 'Random User 48',
+    email: 'random.user.48@gmail.com',
+    image_url: '/customers/emil-kowalski.png'
+  },
+  {
+    id: 'c2b76d6a-9e2a-4865-a098-9db590f6117c',
+    name: 'Random User 49',
+    email: 'random.user.49@outlook.com',
+    image_url: '/customers/amy-burns.png'
+  },
+  {
+    id: '0c54085b-5299-445d-ab05-3d15a22293c1',
+    name: 'Random User 50',
+    email: 'random.user.50@outlook.com',
+    image_url: '/customers/balazs-orban.png'
+  }
 ];
 
 const invoices = [
@@ -162,6 +462,126 @@ const invoices = [
     amount: 1000,
     status: 'paid',
     date: '2022-06-05',
+  },
+  {
+    customer_id: customers[0].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[1].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[2].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[3].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[4].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[5].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[6].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[7].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[8].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[9].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[0].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[1].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[2].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[3].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[4].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[5].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[6].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[7].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[8].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
+  },
+  {
+    customer_id: customers[9].id,
+    amount: 44800,
+    status: 'paid',
+    date: '2023-09-10',
   },
 ];
 
